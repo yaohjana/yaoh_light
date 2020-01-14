b6b698643f400b6ba1be15624e3a5a9b655158c5
Deng Yuan Zheng <yaoh@dcjh.tn.edu.tw>
2020-01-14 16:30:05 +0800

    Add files via upload

--- blocks/yaoh_light_block.php
+++ blocks/yaoh_light_block.php
@@ -8,50 +8,53 @@ include_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'jsloader.inc.php');
  *@para:$edit bool 是否顯示編輯按鈕
  *@para：$options array為設定用陣列
  *		$options[0]共讀取幾則(空白或0為全部讀取)
- *		$options[1]實際顯示幾則(空白或0為全部顯示)
- *		$options[2]幾秒切換一次(空白或0為部切換)
- *		$options[3]旋轉角度(空白或0為正常角度)
- *		$options[4]看板寬度(空白或0則自動調整)
- *		$options[5]背景顏色(預設為空白)
- *		$options[6]顯示的tag(一段文字)
+ *		$options[1]亂數顯示
+ *		$options[2]實際顯示幾則(空白或0為全部顯示)
+ *		$options[3]幾秒切換一次(空白或0為部切換)
+ *		$options[4]旋轉角度(空白或0為正常角度)
+ *		$options[5]看板寬度(空白或0則自動調整)
+ *		$options[6]背景顏色(預設為空白)
+ *		$options[7]顯示的tag(一段文字)
  *@return:區塊顯示的內容
  */
 function list_light_in_blocks($options,$edit=false){
-  global $xoopsDB,$xoopsModuleConfig;
-	if($options[0]>0){
-		$limit_str=" LIMIT 0, ".$options[0];
+	global $xoopsDB,$xoopsModuleConfig;
+	$sql="SELECT * FROM ".$xoopsDB->prefix("yaoh_light").
+		" WHERE find_in_set('{$options[7]}', tag) ".
+		" ORDER BY ";
+	//亂數讀取與否
+	if($options[1]==1){
+		$sql.=" RAND()";
 	}else{
-		$limit_str="";
+		$sql.=" `light_rank`";
 	}
-	
-	$sql="SELECT * FROM ".$xoopsDB->prefix("yaoh_light")." ORDER BY `light_rank` ".$limit_str;
-
+	if($options[0]>0){
+		$sql.=" LIMIT 0, ".$options[0];
+	}
 	$res = $xoopsDB->query($sql) or redirect_header('index.php', 3, mysql_error());
 	//背景色
-	$get_lightboard_color=$options[5];
+	$get_lightboard_color=$options[6];
 	$light='';
 	$edit_txt='';
 
-
 	$pattern='p'.rand();
 	//變動間隔
-	if($options[2]>0){
-		$interval_str="setInterval('$.show{$pattern}()',{$options[2]}000);";
+	if($options[3]>0){
+		$interval_str="setInterval('$.show{$pattern}()',{$options[3]}000);";
 	}else{
 		$interval_str='';
 	}
 
-	if($edit==false)$script_txt=<<<script_txt
-onmouseover="this.setAttribute('scrollamount', 1, 0);" onmouseout="this.setAttribute('scrollamount', 6, 0);"
-script_txt;
+	if($edit==false)$script_txt="
+		onmouseover=\"this.setAttribute('scrollamount', 1, 0);\" onmouseout=\"this.setAttribute('scrollamount', 6, 0);\"";
 		//總量初始
 		$light_num=0;
 		while($row=$xoopsDB->fetchArray($res)){
-			//tag  判讀
-			$tag_array=explode(',', $row['tag']);
-			if($options[6]!=null){
-				if(!in_array($options[6], $tag_array))continue;
-			}
+			//tag  判讀  改由SQL處理
+			// $tag_array=explode(',', $row['tag']);
+			// if($options[7]!=null){
+			// 	if(!in_array($options[7], $tag_array))continue;
+			// }
 			$light_num++;
 			//產生跑馬燈格式
 			
@@ -65,8 +68,8 @@ script_txt;
 			"._MB_MARQUEE_DEL."</a><br><br><br>";
 			}
 		//顯示數量
-		if($options[1]>0){
-			$see_num_limit=$options[1];
+		if($options[2]>0){
+			$see_num_limit=$options[2];
 		}else{
 			$see_num_limit=$light_num;
 		}
@@ -86,8 +89,8 @@ script_txt;
 			{$interval_str};
 			");
 			//揭示板旋轉角度
-		$rotate_deg=$options[3];
-		$lightboard_width=($options[4]>0)?' width:'.$options[4].'px;':'';
+		$rotate_deg=$options[4];
+		$lightboard_width=($options[5]>0)?' width:'.$options[5].'px;':'';
 		$rotate_deg_txt=($rotate_deg<>null)?"-webkit-transform:rotate(".$rotate_deg."deg);-moz-transform:rotate(".$rotate_deg."deg);-ms-transform:rotate(".$rotate_deg."deg);":"";
 		$box_style_txt=$lightboard_width."{$rotate_deg_txt}";
 		$box_style_txt2="background:none repeat scroll 0 0 {$get_lightboard_color};border-radius: 20px 20px 20px 20px;padding: 0 20px; ";
@@ -102,18 +105,20 @@ script_txt;
  *功能：輸出區塊設定的表單
  *@para：$options array為設定用陣列
  *		$options[0]共讀取幾則(空白或0為全部讀取)
- *		$options[1]實際顯示幾則(空白或0為全部顯示)
- *		$options[2]幾秒切換一次(空白或0為部切換)
- *		$options[3]旋轉角度(空白或0為正常角度)
- *		$options[4]看板寬度(空白或0則自動調整)
- *		$options[5]背景顏色(預設為空白)
- *		$options[6]顯示的tag(一段文字)
+ *		$options[1]亂數顯示
+ *		$options[2]實際顯示幾則(空白或0為全部顯示)
+ *		$options[3]幾秒切換一次(空白或0為部切換)
+ *		$options[4]旋轉角度(空白或0為正常角度)
+ *		$options[5]看板寬度(空白或0則自動調整)
+ *		$options[6]背景顏色(預設為空白)
+ *		$options[7]顯示的tag(一段文字)
+
  *@return:輸出區塊設定的表單
  */
 function edit_list_light($options){
 	include_once(XOOPS_ROOT_PATH."/class/xoopsformloader.php");
 	//背景顏色
-	$color_picker=new XoopsFormColorPicker(_MB_MARQUEE_BACKGROUND_COLOR,'options[5]',$options[5]);
+	$color_picker=new XoopsFormColorPicker(_MB_MARQUEE_BACKGROUND_COLOR,'options[6]',$options[6]);
 	$color_picker=$color_picker->render();
 
 	//tag selector
@@ -127,9 +132,9 @@ function edit_list_light($options){
 	$tag_count=array_count_values($tag_array);
 	$tag_options_array=array();
 
-	//想要顯示的標籤
-	$tag_selector=new XoopsFormSelect(_MB_MARQUEE_SHOW_TAG,'options[6]',$options[6],1,false);
-	//全部顯示
+//想要顯示的標籤
+	$tag_selector=new XoopsFormSelect(_MB_MARQUEE_SHOW_TAG,'options[7]',$options[7],1,false);
+//全部顯示
 	$tag_options_array['']=_MB_MARQUEE_SHOW_ALL;
 	foreach($tag_count as $a=>$b){
 		if($a==null)continue;
@@ -137,35 +142,50 @@ function edit_list_light($options){
 	}
 	$tag_selector->addOptionArray($tag_options_array);
 	$tag_selector=$tag_selector->render();
-  $return= "<ol><li>";
-    //共讀取幾則(空白或0為全部讀取)？
-  $return.=_MB_MARQUEE_WHOLE_READ;
-  $return.="<input type='number' name='options[0]' value='{$options[0]}' size=2>
-  </li><br><li>";
-  	//實際顯示幾則(空白或0為全部顯示)？
-  $return.=_MB_MARQUEE_WHOLE_SEE;
-  $return.="<input type='number' name='options[1]' value='{$options[1]}' size=2>
-   </li><br><li>";
-   //幾秒切換一次?
-   $return.=_MB_MARQUEE_CHANGE_FREQUENCE;
-   $return.="<input type='number' name='options[2]' value='{$options[2]}' size=2>
-  </li><br><li>";
-	//旋轉角度(空白或0為正常角度)
-  $return.=_MB_MARQUEE_ROTATE;
-    $return.="<input type='number' name='options[3]' value='{$options[3]}' size=3>
-  </li><br><li>";
-  //看板寬度(空白或0則自動調整)
-  $return.=_MB_MARQUEE_WIDTH;
+	$return= "<ol><li>";
+	
+ //共讀取幾則(空白或0為全部讀取)？
+	$return.=_MB_MARQUEE_WHOLE_READ;
+	$return.="<input type='number' name='options[0]' value='{$options[0]}' size=2>";
+ 
+ //隨機讀取
+	$return.="<input type='checkbox' name='options[1]' value='1' checked='checked'>";
+	$return.=_MB_MARQUEE_RANDOM_SHOW;
+	$return.="</li><br>";
+	
+ //實際顯示幾則(空白或0為全部顯示)？
+	$return.="<li>";
+	$return.=_MB_MARQUEE_WHOLE_SEE;
+	$return.="<input type='number' name='options[2]' value='{$options[2]}' size=2>
+			</li><br>";
+
+ //幾秒切換一次?
+	$return.="<li>";
+	$return.=_MB_MARQUEE_CHANGE_FREQUENCE;
+	$return.="<input type='number' name='options[3]' value='{$options[3]}' size=2>
+			</li><br>";
+			
+//旋轉角度(空白或0為正常角度)
+	$return.="<li>";
+	$return.=_MB_MARQUEE_ROTATE;
     $return.="<input type='number' name='options[4]' value='{$options[4]}' size=3>
-	</li>  <br>
-	<li>";
-	//背景顏色
+			</li><br>";
+			
+//看板寬度(空白或0則自動調整)
+	$return.="<li>";
+	$return.=_MB_MARQUEE_WIDTH;
+    $return.="<input type='number' name='options[5]' value='{$options[5]}' size=3>
+			</li><br>";
+			
+//背景顏色
+	$return.="<li>";
 	$return.=_MB_MARQUEE_BACKGROUND_COLOR;
 	
-	//透明背景
-	$return.=$color_picker."<input type='checkbox' name='options[5]' value='' checked='checked'>"._MB_MARQUEE_TRANS."</li>
-	<li>";
-	//要顯示的tag
+//透明背景
+	$return.=$color_picker."<input type='checkbox' name='options[6]' value='' checked='checked'>"._MB_MARQUEE_TRANS."</li>";
+	
+//要顯示的tag
+	$return.="<li>";
 	$return.=_MB_MARQUEE_THE_TAG;
 	$return.=$tag_selector."</li>  <br>
 	</ol>";
